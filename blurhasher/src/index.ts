3cd7538b9581d239231937208a51805d2918c70f
Anton Kovalev <antonko@gmail.com>
2024-03-15 21:28:02 +0300

    feat!: Settings, regeneration images and refactoring

--- blurhasher/src/index.ts
+++ blurhasher/src/index.ts
@@ -1,30 +1,90 @@
 import { defineHook } from "@directus/extensions-sdk";
 import { runMigration } from "./migration";
 import { generateBlurHashFromStream } from "./blurhash";
+import { getSetting } from "./util";
+import { settings_detail_level, settings_regenerate } from "./fields";
+import { regenerate_all_images } from "./regenerate";
 
 export default defineHook(
   ({ action, init }, { services, database, getSchema, logger }) => {
-    const { AssetsService, ItemsService, FieldsService } = services;
+    const { AssetsService, ItemsService, FieldsService, SettingsService } =
+      services;
+
+    /**
+     * Extension initialization hook.
+     * This hook is called when the extension is loaded.
+     **/
     init("routes.custom.after", async () => {
       logger.info(`[blurhasher]: Initializing extension`);
       const schema = await getSchema();
       const fieldsService = new FieldsService({ knex: database, schema });
-      runMigration(fieldsService, logger);
+      /**
+       * Migration
+       */
+      await runMigration(fieldsService, logger);
+
+      const settings = new SettingsService({ schema, knex: database });
+      const detail_level = await getSetting(
+        settings,
+        settings_detail_level.field,
+      );
+      const is_regenerate = await getSetting(
+        settings,
+        settings_regenerate.field,
+      );
+      if (!is_regenerate) {
+        return;
+      }
+      /**
+       * Regenerate all blurhashes if the setting is enabled.
+       */
+      try {
+        logger.warn("[blurhasher]: Regenerating blurhashes");
+
+        const itemsService = new ItemsService("directus_files", {
+          knex: database,
+          schema: schema,
+        });
+        const assetsService = new AssetsService({
+          knex: database,
+          schema: schema,
+        });
+        await regenerate_all_images(
+          itemsService,
+          assetsService,
+          detail_level,
+          logger,
+        );
+        logger.warn("[blurhasher]: Regeneration complete");
+      } catch (error) {
+        logger.error(
+          `[blurhasher]: An error occurred while regenerating blurhashes: ${error}`,
+        );
+      } finally {
+        settings.upsertSingleton({ blurhasher_regenerate_on_restart: false });
+      }
     });
 
+    /**
+     * Action hook for the `files.upload` action.
+     * This hook is called when a file is uploaded to Directus.
+     */
     action("files.upload", async ({ payload, key }, context) => {
       if (
         !["image/jpeg", "image/png", "image/webp", "image/tiff"].includes(
           payload.type,
         )
       ) {
-        logger.info("[blurhasher]: The file is not a image - skipped");
+        // Skip non-image files
         return;
       }
 
-      const serviceOptions = { ...context, knex: context.database };
-      const assetsService = new AssetsService(serviceOptions);
-      const itemService = new ItemsService("directus_files", {
+      const assetsService = new AssetsService({
+        ...context,
+        knex: context.database,
+      });
+
+      const itemsService = new ItemsService("directus_files", {
         knex: database,
         schema: context.schema,
       });
@@ -32,13 +92,29 @@ export default defineHook(
       const { stream } = await assetsService.getAsset(key, {
         transformationParams: {
           quality: 80,
-          width: 200,
+          width: 100,
           fit: "inside",
         },
       });
-      generateBlurHashFromStream(stream).then((blurHash) => {
-        itemService.updateOne(key, { blurhash: blurHash });
+
+      const settings = new SettingsService({
+        schema: context.schema,
+        knex: database,
       });
+      const detail_level = await getSetting(
+        settings,
+        settings_detail_level.field,
+      );
+
+      /**
+       * Generate the blurhash from the stream and update the item.
+       */
+      const blurHash: string | null = await generateBlurHashFromStream(
+        stream,
+        detail_level,
+        logger,
+      );
+      itemsService.updateOne(key, { blurhash: blurHash });
     });
   },
 );
--- blurhasher/src/migration.ts
+++ blurhasher/src/migration.ts
@@ -6,12 +6,26 @@ import {
   settings_regenerate,
 } from "./fields";
 
+/**
+ * Runs the migration process.
+ *
+ * @param fieldsService - The service for managing fields.
+ * @param logger - The logger for logging messages.
+ */
 export async function runMigration(fieldsService: any, logger: Logger) {
   await ensureField(fieldsService, directus_files_blurhash, logger);
   await ensureField(fieldsService, settings_detail_level, logger);
   await ensureField(fieldsService, settings_regenerate, logger);
 }
 
+/**
+ * Ensures the existence of a field in Directus.
+ * If the field does not exist, it creates the field using the provided field configuration.
+ *
+ * @param fieldsService - The Directus fields service.
+ * @param field - The field configuration to ensure.
+ * @param logger - The logger instance for logging messages.
+ */
 export async function ensureField(
   fieldsService: any,
   field: FieldRaw,
